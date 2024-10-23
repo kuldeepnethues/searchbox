226800fd84efb4bcbe6ca72606239b7ccc389840
kuldeepnethues <kuldeepguptanethues@gmail.com>
2024-10-23 17:47:09 +0530

    added hosts discovery

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
+import pandas as pd
 from stingbox import stingboxinfo
-from stingboxapi.api import data, all_boxes, alerts
+from stingboxapi.api import data, all_boxes, alerts, hosts
 
 st.set_page_config(
     page_title="StingBox",
@@ -22,12 +23,35 @@ if code in all_boxes:
     stingboxinfo.stingboxinfo(data, i)
 
     st.subheader('Alerts')
-    for alert in alerts:
-        if alert['name'] == data[i]['name']:
-            st.write(f"{alert['ts']} : {alert['alert_text']}")
-
-
+    results = []
+    for j in range(len(data)):
+        for alert in alerts:
+            if alert['name'] == data[i]['name']:
+                results.append({
+                    'DateTime': alert['ts'],
+                    'Alert': alert['alert_text']                                 
+                })
+    if results:  # Check if results list is not empty
+        df = pd.DataFrame(results)
+        st.write(df)
+    else: st.write('No recent alerts found')
+                
     st.subheader('Discovered Hosts')
+    results = []
+    for j in range(len(data)):
+        for host in hosts:
+            if host['name'] == data[i]['name']:
+                results.append({
+                    'IP Address': host['ip'],
+                    'MAC Address': host['mac'],
+                    'Description': host['description'],
+                    'Custom Name': host['custom_description'],
+                    'Last Seen': host['ts']
+                    })
+    if results:  # Check if results list is not empty
+        df = pd.DataFrame(results)
+        st.write(df)
+    else: st.write('No Active Hosts Found')
 
 else:
     st.write('Not found')
